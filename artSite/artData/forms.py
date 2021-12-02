3b7f2568dd881fa5c819bbcffe49b2eed3ea9bb8
srh95 <srh95@case.edu>
2021-12-02 18:49:30 -0500

    data entry and table viewing

--- artSite/artData/forms.py
+++ artSite/artData/forms.py
@@ -5,3 +5,4 @@ class CustomerForm(forms.ModelForm):
     class Meta:
         model = CustomerModel
         fields = ["customer_id", "first_name", "last_name", "street_num","street_name", "city", "state", "zip_code", "preferred_style", "preferred_medium", "phone_num", "artist_id"]
+
--- artSite/artData/views.py
+++ artSite/artData/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import HttpResponse
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .models import CustomerModel
 from .forms import CustomerForm
 
-
-# Create your views here.
-def index(request):
-    return HttpResponse("Hello world")
 
 def display(request):
     cust = CustomerModel.objects.all()
@@ -17,3 +13,79 @@ def cust_form(request):
         form.save()
     context = {'form':form}
     return render(request, 'customer.html', context)
+
+def homepage(request):
+    return render(request, 'homepage.html')
+
+def dataEntry(request):
+    return render(request, 'dataEntry.html')
+
+def artist(request):
+    return render(request, 'artist.html')
+
+def addArtist(request):
+    return render(request, 'addArtist.html')
+
+def artwork(request):
+    artworks = ArtworkModel.objects.all()
+    return render(request, 'artwork.html',{'artworks': artworks})
+
+def collector(request):
+    return render(request, 'collector.html')
+
+def customer(request):
+    customers = CustomerModel.objects.all()
+    return render(request, 'customer.html',{'customers': customers})
+
+def addCustomer(request):
+    if request.method == 'POST':
+        form = CustomerForm(request.POST)
+        if form.is_valid():
+            # if ~(Artist.objects.get(id=form.cleaned_data['artistid'])):
+            #     messages.error(request, 'artist does not exist')
+            #     return HttpResponseRedirect('/addCustomer')
+
+            database = CustomerModel.objects.create(
+                first_name = form.cleaned_data['firstname'],
+                last_name = form.cleaned_data['lastname'],
+                street_num = form.cleaned_data['streetnum'],
+                street_name = form.cleaned_data['streetname'],
+                city = form.cleaned_data['city'],
+                state = form.cleaned_data['state'],
+                zip_code = form.cleaned_data['zipocde'],
+                preferred_style = form.cleaned_data['style'],
+                preferred_medium = form.cleaned_data['medium'],
+                phone_num = form.cleaned_data['phone'],
+                artist_id = form.cleaned_data['artistid']
+            )
+            database.save()
+            return HttpResponseRedirect('/customer')
+    else:
+        form = CustomerForm()
+
+    return render(request, 'addCustomer.html', {'form': form})
+
+def artshow(request):
+    artshows = ArtShowModel.objects.all()
+    return render(request, 'artshow.html', {'artshows': artshows})
+
+def buyer(request):
+    return render(request, 'buyer.html')
+
+def renter(request):
+    return render(request, 'renter.html')
+
+def rent(request):
+    return render(request, 'rent.html')
+
+def sale(request):
+    return render(request, 'sale.html')
+
+def displayed(request):
+    return render(request, 'displayed.html')
+
+def sold(request):
+    return render(request, 'sold.html')
+
+def rented(request):
+    return render(request, 'rented.html')
